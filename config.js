c32596229dbf41773c6003370c3c0ba78b0b7f2e
Rollings-mude <rollingstogara08@gmail.com>
2025-06-24 16:52:34 +0200

    Update config.js

--- config.js
+++ config.js
@@ -6,8 +6,8 @@ const toBool = (value) => value === "true";
 
 // Owner & Bot Details
 global.owner = process.env.OWNER_NUMBER;
-global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.ownername = process.env.OWNER_NAME || "David Cyril";
+global.anitav4 = process.env.BOT_NAME || "DEITY MUDE";
+global.ownername = process.env.OWNER_NAME || "ROLLINGS";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
